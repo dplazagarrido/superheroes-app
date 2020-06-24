a397cfa22aeb42f5554ff12ae0e01fa3e1ade930
Diego Plaza <d.plazagarrido@gmail.com>
2020-06-23 21:31:23 -0400

    feat: change route name and redirect

--- src/router/index.js
+++ src/router/index.js
@@ -8,15 +8,15 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
-    component: Home
+    redirect: '/superheroes',
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/superheroes',
+    name: 'SuperHeroes',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../components/superHeroesList')
   }
 ]
 
