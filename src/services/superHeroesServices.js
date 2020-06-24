c6e57863d77ba6b1b58e2ed33a3d5ec830123df3
Diego Plaza <d.plazagarrido@gmail.com>
2020-06-23 20:41:53 -0400

    feat: add superHeroes services

---
+++ src/services/superHeroesServices.js
@@ -0,0 +1,14 @@
+import { superHeroesAppis } from '../utils/apisUrlConfig'
+import axios from 'axios'
+
+export function getAllSuperHeroes () {
+  return axios.get(superHeroesAppis + '/v1/test/superheroes')
+}
+
+export function getSuperHeroeById (superHeroeId) {
+  return axios.get(superHeroesAppis + '/v1/test/superheroes/' + superHeroeId)
+}
+
+export function getFlySuperHeroes (value) {
+  return axios.get(superHeroesAppis + '/v1/test/superheroes/?puedeVolar=' + value)
+}
